4d2831d60e3acd91192627fd03d4ebe4c48fd02a
patrickb <pm.bijker@gmail.com>
2016-01-26 20:06:35 +0100

    Updated app layout

--- app/src/main/java/eu/ict/traveltimeregistration/MainActivity.java
+++ app/src/main/java/eu/ict/traveltimeregistration/MainActivity.java
@@ -1,15 +1,45 @@
 package eu.ict.traveltimeregistration;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_STORAGE = 1;
+
+    private Spinner mBluetoothDeviceSpinner;
+
+    private final String NO_BT_DEVICE = "None";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +48,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        requestPermissions();
+
+        createUiElements();
     }
 
     @Override
@@ -48,5 +73,77 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void createUiElements() {
+        Set<BluetoothDevice> availableDevices = BluetoothAdapter.getDefaultAdapter().getBondedDevices();
+        mBluetoothDeviceSpinner = (Spinner) findViewById(R.id.availableBtDevices);
+        ArrayList<String> arrayList= new ArrayList<String>();
+        arrayList.add(NO_BT_DEVICE);
+        if(availableDevices != null) {
+            for(BluetoothDevice dev : availableDevices) {
+                arrayList.add(dev.getName());
+            }
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, arrayList);
+        mBluetoothDeviceSpinner.setAdapter(arrayAdapter);
+        mBluetoothDeviceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView mTextView = (TextView)findViewById(R.id.connectHistory);
+                String selectedDevice = (String)mBluetoothDeviceSpinner.getAdapter().getItem(position);
+                if(selectedDevice.equalsIgnoreCase(NO_BT_DEVICE)){
+                    mTextView.setText(R.string.clearHistory);
+                }
+                else {
+                    // Read connect/disconnect event file for this device
+                    mTextView.setText("");
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                TextView mTextView = (TextView)findViewById(R.id.connectHistory);
+                mTextView.setText(R.string.clearHistory);
+            }
+        });
+
+    }
+
+    private void requestPermissions(){
+        if(this.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if(!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_STORAGE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                    AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                    alertDialog.setTitle("Permission denied!!");
+                    alertDialog.setMessage("The application cannot write to SDcard withouth the permission.\nApp will be closed.");
+                    alertDialog.setButton(DialogInterface.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                            System.exit(0);
+                        }
+                    });
+                    // Showing Alert Message
+                    alertDialog.show();
+                }
+                return;
+            }
+        }
     }
 }
